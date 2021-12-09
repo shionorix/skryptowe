455402a55c60d8e0c6bebb2722d7fca0a891c484
shionorix <91876400+shionorix@users.noreply.github.com>
2021-12-09 20:56:54 +0100

    Delete zad1 directory

--- zad1/script.js
+++
@@ -1,4 +0,0 @@
-function printdata() {
-    alert(document.forms['simpleform'].pole_tekstowe.value + ' - ' + typeof(document.forms['simpleform'].pole_tekstowe.value) + '\n' + document.forms['simpleform'].pole_liczbowe.value + ' - ' + typeof(document.forms['simpleform'].pole_liczbowe.value));
-}  
-document.forms['simpleform'].wypisz.onclick = printdata
