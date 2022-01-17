bd68854bffb034a37d1f7899249a2c634b85befb
shionorix <91876400+shionorix@users.noreply.github.com>
2022-01-17 12:17:10 +0100

    Delete lab8/zajecia directory

--- lab8/zajecia/script.js
+++
@@ -1,7 +0,0 @@
-function wykonaj() {
-    var input = document.forms[0].elements.tekst.value;
-    var storage = document.forms[0].elements.webstorage.checked;
-    console.log(input, storage);
-}
-
-document.forms[0].elements.wykonaj.onclick = wykonaj;
